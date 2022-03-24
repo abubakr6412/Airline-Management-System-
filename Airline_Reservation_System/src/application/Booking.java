75790d01e0915b61461c1601e85698fe49f17ba5
AbuBakr Humayun <galaxyte6412@gmail.com>
2022-03-24 18:45:35 +0500

    Final AMS Sprint 1

---
+++ Airline_Reservation_System/src/application/Booking.java
@@ -0,0 +1,49 @@
+package application;
+
+public class Booking {
+	private int Booking_ID,Customer_ID,Num_Seats,Flight_ID;
+
+	
+	
+	Booking(int BID,int CID,int Numseats,int FiD){
+		Booking_ID=BID;
+		Customer_ID=CID;
+		Num_Seats=Numseats;
+		Flight_ID=FiD;
+		
+	}
+	public int getCustomer_ID() {
+		return Customer_ID;
+	}
+
+	public void setCustomer_ID(int customer_ID) {
+		Customer_ID = customer_ID;
+	}
+
+	public int getNum_Seats() {
+		return Num_Seats;
+	}
+
+	public void setNum_Seats(int num_Seats) {
+		Num_Seats = num_Seats;
+	}
+
+	public int getFlight_ID() {
+		return Flight_ID;
+	}
+
+	public void setFlight_ID(int flight_ID) {
+		Flight_ID = flight_ID;
+	}
+	public int getBooking_ID() {
+		return Booking_ID;
+	}
+	public void setBooking_ID(int booking_ID) {
+		Booking_ID = booking_ID;
+	}
+	
+	public void PrintBooking() {
+		System.out.println("Booking ID: "+Booking_ID+" Customer ID: "+Customer_ID+" Num Seats: "+Num_Seats+" Flight ID: "+Flight_ID);
+	}
+	
+}
---
+++ Airline_Reservation_System/src/application/Seat.java
@@ -0,0 +1,36 @@
+package application;
+
+public class Seat {
+
+	
+	private int Booking_ID,Seat_No;
+	private String Seat_Type;
+	
+	Seat(int BID,int SNo,String Stype){
+		Booking_ID=BID;
+		Seat_No=SNo;
+		Seat_Type=Stype;
+	}
+	public int getBooking_ID() {
+		return Booking_ID;
+	}
+	public void setBooking_ID(int booking_ID) {
+		Booking_ID = booking_ID;
+	}
+	public int getSeat_No() {
+		return Seat_No;
+	}
+	public void setSeat_No(int seat_No) {
+		Seat_No = seat_No;
+	}
+	public String getSeat_Type() {
+		return Seat_Type;
+	}
+	public void setSeat_Type(String seat_Type) {
+		Seat_Type = seat_Type;
+	}
+	
+	public void PrintSeat() {
+		System.out.println("Booking ID: "+Booking_ID+" Seat No: "+Seat_No+" Seat Type: "+Seat_Type);
+	}
+}
